28c17fbf37df6df2509ea4a142969b5d56767eae
Ben Cullen <benjaminjcullen1@gmail.com>
2021-02-08 18:29:44 +0000

    infite scroll from unsplash

--- script.js
+++ script.js
@@ -1,19 +1,73 @@
-
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
 
 const apiKey = 'Qgwr0WvK02kXTq6cQLktlNL57kQjzKNYtHLTUvArC8I';
-const count = 10;
+const count = 30;
 
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+function imageLoaded() {
+    
+    imagesLoaded++;
+    console.log(imagesLoaded);
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
+
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+        }
+    }
+
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+href: photo.links.html,
+target: '_blank',
+        });
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
+        img.addEventListener('load', imageLoaded);
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
 
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos();
+        
     } catch (error) {
         
     }
 }
+
+
+// Check to see if scroling near bottom of the page = load more photos.
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        getPhotos();
+        ready = false;
+    }
+})
 
 getPhotos();
